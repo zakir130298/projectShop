24fc9acf67cafe99f7c994987e557d678b51c58a
zakir130298 <zakir130298@gmail.com>
2023-10-06 22:13:41 +0500

    third commit

--- ShopOnline/settings.py
+++ ShopOnline/settings.py
@@ -79,14 +79,20 @@ WSGI_APPLICATION = 'ShopOnline.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'shoponline',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Zb123456',
+#         'HOST': 'localhost',
+#         'PORT': 5432
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'shoponline',
-        'USER': 'postgres',
-        'PASSWORD': 'Zb123456',
-        'HOST': 'localhost',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
